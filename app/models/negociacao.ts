c0f63676da2f687fc22513014222d947bc7e0e0d
Nicolas Leonardo Miranda Lima <nicolas.lima@in.cassems.com.br>
2022-07-22 13:10:17 -0400

    classe de negociações para listar e adicionar as negociaçoes em um array

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,13 +1,28 @@
 export class Negociacao{
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
 
-    constructor(data: Date, quantidade: number, valor: number){
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+    constructor(
+        private _data: Date,
+        public readonly quantidade: number,
+        public readonly valor: number
+        ) {}
+
+    get data(): Date {
+        const data = new Date(this._data.getTime());
+        return data;
     }
+
+    get volume(): number{
+        return this.quantidade * this.valor;
+    }
+
+
+    // o codigo abaixo é igual ao de cima //
+
+    /*constructor(
+        private _data: Date,
+        private _quantidade: number,
+        private _valor: number
+        ){}
 
     get data(): Date{
         return this._data;
@@ -20,5 +35,5 @@ export class Negociacao{
     }
     get volume(): number{
         return this._quantidade * this._valor;
-    }
+    } */
 }
--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,12 +1,15 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 export class NegociacaoController {
     constructor() {
+        this.negociacoes = new Negociacoes();
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
     adiciona() {
         const negociacao = this.criaNegociacao();
+        negociacao.data.setDate(11);
         console.log(negociacao);
     }
     criaNegociacao() {
@@ -15,5 +18,11 @@ export class NegociacaoController {
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
         return new Negociacao(date, quantidade, valor);
+    }
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
     }
 }
